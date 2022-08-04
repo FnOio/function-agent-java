1df0db6508929f3f0e655e5589a0c72a3104278a
Robbe Van Rijsselberghe <robbe.vanrijsselberghe@ugent.be>
2022-08-04 10:40:51 +0200

    wip: better javadoc

--- src/main/java/be/ugent/idlab/knows/functions/agent/functionIntantiation/ThrowableFunction.java
+++ src/main/java/be/ugent/idlab/knows/functions/agent/functionIntantiation/ThrowableFunction.java
@@ -3,7 +3,7 @@ package be.ugent.idlab.knows.functions.agent.functionIntantiation;
 import be.ugent.idlab.knows.functions.agent.Agent;
 
 /**
- * interface to describe a function that can throw an {@link Exception}
+ * interface to describe a function that takes an {@link Agent} and a number of {@link Object}s and can throw an {@link Exception}
  */
 @FunctionalInterface
 public interface ThrowableFunction{
