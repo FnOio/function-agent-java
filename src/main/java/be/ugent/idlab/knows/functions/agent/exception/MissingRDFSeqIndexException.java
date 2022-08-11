75015a283965919a7478bce173de493fcdb255a4
Robbe Van Rijsselberghe <robbe.vanrijsselberghe@ugent.be>
2022-08-11 15:38:51 +0200

    wip: add abstract error class

--- src/main/java/be/ugent/idlab/knows/functions/agent/exception/MissingRDFSeqIndexException.java
+++ src/main/java/be/ugent/idlab/knows/functions/agent/exception/MissingRDFSeqIndexException.java
@@ -1,6 +1,6 @@
 package be.ugent.idlab.knows.functions.agent.exception;
 
-public class MissingRDFSeqIndexException extends Exception{
+public class MissingRDFSeqIndexException extends AgentException{
     public MissingRDFSeqIndexException(String message) {
         super(message);
     }
