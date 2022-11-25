d34aad8ca42ac98606da6b7991a8a053925eba35
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2022-11-25 11:42:29 +0100

    Code review JarFileTest.java: auto-close JarFile resource

--- src/test/java/be/ugent/idlab/knows/misc/JarFileTest.java
+++ src/test/java/be/ugent/idlab/knows/misc/JarFileTest.java
@@ -20,27 +20,28 @@ public class JarFileTest {
     public void testExtractClassesFromJar() throws IOException {
         String jarFilePath = "./src/test/resources/GrelFunctions.jar";
         //String jarFilePath = "/home/gerald/projects/rml/rml-streamer/target/RMLStreamer-2.2.2.jar";
-        JarFile jarFile = new JarFile(jarFilePath);
-        System.out.println("name: " + jarFile.getName());
-        try (URLClassLoader cl = URLClassLoader.newInstance(
-                new URL[] { new URL("jar:file:" + jarFilePath + "!/") })) {
-            jarFile
-                    .stream()
-                    .map(ZipEntry::getName)
-                    .filter(name -> name.endsWith(".class") && !name.contains("$"))
-                    .map(name -> name.substring(0, name.lastIndexOf('.')).replaceAll("/", "."))
-                    .forEach(className -> {
-                        System.out.println("class: " + className);
-                        try {
-                            Class<?> clazz = cl.loadClass(className);
-                            Method[] methods = clazz.getDeclaredMethods();
-                            for (Method method : methods) {
-                                System.out.println("  " + method.getName());
+        try (JarFile jarFile = new JarFile(jarFilePath)) {
+            System.out.println("name: " + jarFile.getName());
+            try (URLClassLoader cl = URLClassLoader.newInstance(
+                    new URL[]{new URL("jar:file:" + jarFilePath + "!/")})) {
+                jarFile
+                        .stream()
+                        .map(ZipEntry::getName)
+                        .filter(name -> name.endsWith(".class") && !name.contains("$"))
+                        .map(name -> name.substring(0, name.lastIndexOf('.')).replaceAll("/", "."))
+                        .forEach(className -> {
+                            System.out.println("class: " + className);
+                            try {
+                                Class<?> clazz = cl.loadClass(className);
+                                Method[] methods = clazz.getDeclaredMethods();
+                                for (Method method : methods) {
+                                    System.out.println("  " + method.getName());
+                                }
+                            } catch (ClassNotFoundException e) {
+                                e.printStackTrace();
                             }
-                        } catch (ClassNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    });
+                        });
+            }
         }
 
     }
