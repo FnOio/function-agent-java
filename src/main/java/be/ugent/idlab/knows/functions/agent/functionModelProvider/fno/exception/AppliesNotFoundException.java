8e760e0799ce4bcab160396a5ac3e49b17af8039
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2022-11-25 10:29:37 +0100

    Code review FnOFunctionModelProvider
    - throw meaningful exception when object for certain subject + object is not found

---
+++ src/main/java/be/ugent/idlab/knows/functions/agent/functionModelProvider/fno/exception/AppliesNotFoundException.java
@@ -0,0 +1,12 @@
+package be.ugent.idlab.knows.functions.agent.functionModelProvider.fno.exception;
+
+/**
+ * <p>Copyright 2022 IDLab (Ghent University - imec)</p>
+ *
+ * @author Gerald Haesendonck
+ */
+public class AppliesNotFoundException extends FunctionCompositionException {
+    public AppliesNotFoundException(String message) {
+        super(message);
+    }
+}
